f253071b693553ecec89bd6fc0b77a9b0d38accf
Micha Pierzchala <michal.pierzchala99@poczta.fm>
2021-01-27 20:29:24 +0100

    Added basic authorisation framework

--- public/views/login.php
+++ public/views/login.php
@@ -9,18 +9,26 @@
             <img src="public/img/logo.svg">
         </div>
         <div class="login-container">
-            <form class="form_all">
+            <form class="form_all" action="login" method="POST">
                 <div class="login">
+                    <div class="messages">
+                        <?php if(isset($messages)){
+                            foreach ($messages as $message) {
+                                echo $message;
+                            }
+                        }
+                        ?>
+                    </div>
                     <p class="login_p1">Username</p>
-                    <input class="login_field" name="login" type="email" placeholder="__________" >
+                    <input class="login_field" name="email" type="email" placeholder="__________" >
                     <p class="login_p">Password</p>
                     <input class="login_field" name="password" type="password" placeholder="__________">
                 </div>
                 
-                <button type="button" class="btn vertical-margin-127px margin-left-auto">LOGIN</button>
+                <button type="submit" class="btn vertical-margin-127px margin-left-auto">LOGIN</button>
                 <div class="login-create">
                     <p class="login_question">Don't have an account?</p>
-                    <button type="button" class="btn btn-create">Sign in</button>
+                    <button type="submit" class="btn btn-create">Sign in</button>
                 </div>
                 
             </form>
